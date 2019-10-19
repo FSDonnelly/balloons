b7da5bfc9767ea363ba34fee376df46146a93720
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-19 14:26:44 -0700

    add js for balloon

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 let colors = ['yellow', 'blue', 'red', 'violet', 'green'];
+let windowWidth = window.innerWidth;
+let body = document.body;
 
 createBalloon = () => {
   let div = document.createElement('div');
   let randColor = Math.floor(Math.random() * colors.length);
   div.className = `balloon balloon-${colors[randColor]}`;
-  document.body.appendChild(div);
+
+  let randNum = Math.floor(Math.random() * (windowWidth - 100));
+  div.style.left = `${randNum}px`;
+  body.appendChild(div);
 };
